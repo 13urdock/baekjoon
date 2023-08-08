478909966edfa66880d8fe058ef35841abb19c1b
burdock <115214091+13urdock@users.noreply.github.com>
2023-08-08 22:59:18 +0900

    Update and rename 꼬마 정민.cc to 꼬마 정민.c

--- 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
-    long long a, b, c;
+    long long a, b, c; //범위가 1 < a, b, c <= 10^12 이기 때문에 long long 자료형을 써야함
     scanf("%lld %lld %lld", &a, &b, &c);
 
     printf("%lld", a + b + c);
