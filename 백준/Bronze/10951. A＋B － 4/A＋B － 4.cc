9775428011ac2df1b3ef0154e7f3a290e15efb92
burdock <115214091+13urdock@users.noreply.github.com>
2023-08-12 23:18:04 +0900

    Update A＋B － 4.c

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -2,6 +2,7 @@
 #include <stdio.h>
 
 int main(void) {
+    //이 문제에서는 테스트 케이스를 입력받지 않음. 인풋들을 넣고 그게 끝날 때까지 출력하는 ? 거라? end of file까지 while문 돌리는 거
     int a, b;
     while (scanf("%d %d", &a, &b) != EOF)
         printf("%d\n", a + b);
