522e2ef9147e701122022c451f8a86f5477e690b
burdock <115214091+13urdock@users.noreply.github.com>
2023-08-13 22:05:26 +0900

    [Bronze V] Title: 개수 세기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.cc
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main(void) {
+    int n;
+    scanf("%d", &n);
+
+    int arr[201] = { 0 };
+
+    for (int i = 0; i < n; i++) {
+        int number;
+        scanf("%d", &number);
+        arr[number + 100]++;
+    }
+
+    int pick;
+    scanf("%d", &pick);
+
+    printf("%d", arr[pick + 100]);
+    return 0;
+}
